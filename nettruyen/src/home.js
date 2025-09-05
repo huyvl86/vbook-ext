108770b6ac34963a5dd44b5e07177c075988b8b9
huyvl86 <129029294+huyvl86@users.noreply.github.com>
2025-09-05 16:29:36 +0700

    Update home.js

--- nettruyen/src/home.js
+++ nettruyen/src/home.js
@@ -1,13 +1,13 @@
 load('config.js');
 function execute() {
     return Response.success([
-        {title: "Mới cập nhật", input: BASE_URL + "/tim-kiem-nang-cao&sort=0", script: "gen.js"},
-        {title: "Truyện mới", input: BASE_URL + "/tim-kiem-nang-cao&sort=1", script: "gen.js"},
-        {title: "Top all", input: BASE_URL + "/tim-kiem-nang-cao&sort=2", script: "gen.js"},
-        {title: "Top tháng", input: BASE_URL + "/tim-kiem-nang-cao&sort=3", script: "gen.js"},
-        {title: "Top tuần", input: BASE_URL + "/tim-kiem-nang-cao&sort=4", script: "gen.js"},
-        {title: "Top ngày", input: BASE_URL + "/tim-kiem-nang-cao&sort=5", script: "gen.js"},
-        {title: "Theo dõi", input: BASE_URL + "/tim-kiem-nang-cao&sort=6", script: "gen.js"},
-        {title: "Bình luận", input: BASE_URL + "/tim-kiem-nang-cao&sort=7", script: "gen.js"}
+        {title: "Mới cập nhật", input: BASE_URL + "tim-truyen?status=", script: "gen.js"},
+        {title: "Truyện mới", input: BASE_URL + "tim-truyen?status=&sort=15", script: "gen.js"},
+        {title: "Top all", input: BASE_URL + "/tim-truyen?status=&sort=10", script: "gen.js"},
+        {title: "Top tháng", input: BASE_URL + "/tim-truyen?status=&sort=11", script: "gen.js"},
+        {title: "Top tuần", input: BASE_URL + "/tim-truyen?status=&sort=12", script: "gen.js"},
+        {title: "Top ngày", input: BASE_URL + "/tim-truyen?status=&sort=13", script: "gen.js"},
+        {title: "Theo dõi", input: BASE_URL + "/tim-truyen?status=&sort=20", script: "gen.js"},
+        {title: "Bình luận", input: BASE_URL + "/tim-truyen?status=&sort=25", script: "gen.js"}
     ]);
 }
