963ed8d8ce4605f1cb61425ae69b340ec27dce27
huyvl86 <129029294+huyvl86@users.noreply.github.com>
2025-09-15 02:28:11 +0700

    Update home.js

--- nettruyen/src/home.js
+++ nettruyen/src/home.js
@@ -1,16 +1,16 @@
 function execute() {
     return Response.success([
-        {title: "Mới Cập Nhật", input: "https://www.nettruyentv.com/tim-truyen", script: "gen.js"},
-        {title: "Top Yêu Thích", input: "https://www.nettruyentv.com/tim-truyen?status=-1&sort=20", script: "gen.js"},
-        {title: "Truyện mới", input: "https://www.nettruyentv.com/tim-truyen?status=-1&sort=15", script: "gen.js"},
-        {title: "Top All", input: "https://www.nettruyentv.com/tim-truyen?status=-1&sort=10", script: "gen.js"},
-        {title: "Top Tháng", input: "https://www.nettruyentv.com/tim-truyen?status=-1&sort=11", script: "gen.js"},
-        {title: "Top Tuần", input: "https://www.nettruyentv.com/tim-truyen?status=-1&sort=12", script: "gen.js"},
-        {title: "Top Ngày", input: "https://www.nettruyentv.com/tim-truyen?status=-1&sort=13", script: "gen.js"},
-        {title: "Xuyên Không", input: "https://www.nettruyentv.com/tim-truyen/xuyen-khong-205", script: "gen.js"},
-        {title: "Chuyển Sinh", input: "https://www.nettruyentv.com/tim-truyen/chuyen-sinh-2130", script: "gen.js"},
-        {title: "Ngôn Tình", input: "https://www.nettruyentv.com/tim-truyen/ngon-tinh", script: "gen.js"},
-        {title: "18+", input: "https://www.nettruyentv.com/tim-truyen/truong-thanh", script: "gen.js"},
-        {title: "Đã Hoàn Thành", input: "https://www.nettruyentv.com/truyen-full", script: "gen.js"},
+        {title: "Mới Cập Nhật", input: "https://nettruyenzz.net/tim-truyen", script: "gen.js"},
+        {title: "Top Yêu Thích", input: "https://nettruyenzz.net/tim-truyen?status=-1&sort=20", script: "gen.js"},
+        {title: "Truyện mới", input: "https://nettruyenzz.net/tim-truyen?status=-1&sort=15", script: "gen.js"},
+        {title: "Top All", input: "https://nettruyenzz.net/tim-truyen?status=-1&sort=10", script: "gen.js"},
+        {title: "Top Tháng", input: "https://nettruyenzz.net/tim-truyen?status=-1&sort=11", script: "gen.js"},
+        {title: "Top Tuần", input: "https://nettruyenzz.net/tim-truyen?status=-1&sort=12", script: "gen.js"},
+        {title: "Top Ngày", input: "https://nettruyenzz.net/tim-truyen?status=-1&sort=13", script: "gen.js"},
+        {title: "Xuyên Không", input: "https://nettruyenzz.net/tim-truyen/xuyen-khong-205", script: "gen.js"},
+        {title: "Chuyển Sinh", input: "https://nettruyenzz.net/tim-truyen/chuyen-sinh-2130", script: "gen.js"},
+        {title: "Ngôn Tình", input: "https://nettruyenzz.net/tim-truyen/ngon-tinh", script: "gen.js"},
+        {title: "18+", input: "https://nettruyenzz.net/tim-truyen/truong-thanh", script: "gen.js"},
+        {title: "Đã Hoàn Thành", input: "https://nettruyenzz.net/truyen-full", script: "gen.js"},
     ]);
 }
